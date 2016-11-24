e834aac8061b69ab8cb24e9c49c41493361add05
zhangjie <zhangjie@unissoft-nj.com>
2016-11-24 17:17:57 +0800

    cheak whether all the games end

--- gameData.py
+++ gameData.py
@@ -31,6 +31,9 @@ class SinaGameDataGetter(GameDataGetter):
     """a game data getter of sina sport"""
     url = "http://api.sports.sina.com.cn/?p=nba&s=match&a=dateMatches&format=json&callback=NBA_JSONP_CALLBACK&date=%s&dpc=1"
     date = strftime("%Y-%m-%d", localtime())
+    all_end = False
+    all_data = None
+    summary_data = None
 
     def __init__(self, date=None):
         """format the url with local time"""
@@ -38,9 +41,15 @@ class SinaGameDataGetter(GameDataGetter):
         if date != None:
             self.date = date
         self.url = self.url % self.date
+        # self.raw_data = self.get_games_summary()
+        # self.extract_summary_data(self.raw_data)
 
-    def get_games_summary(self, time=None):
+    def get_games_summary(self):
         """get data summary from sina sport"""
+        #if all games end, don't need to get data from internet again
+        if self.all_end == True:
+            return summary_data
+        #get data from internet and extract useful information
         try:
             #send request to get data
             raw_data = requests.get(self.url).text
@@ -48,9 +57,10 @@ class SinaGameDataGetter(GameDataGetter):
             begin = raw_data.index('(') + 1
             end = raw_data.rindex(')')
             #convert string json data to python dict
-            json_data = json.loads(raw_data[begin:end])
+            self.all_data = json.loads(raw_data[begin:end])
             #etract useful data
-            summary = self.extract_summary_data(json_data)
+            summary = self.extract_summary_data(self.all_data)
+            summary_data = summary
         except Exception as e:
             #print 'meetin error with get games summary data'
             print e
@@ -62,31 +72,48 @@ class SinaGameDataGetter(GameDataGetter):
             }
         return summary
 
-
     def extract_summary_data(self, data):
         """extract useful information from param 'data' which is a dict"""
         games = data['result']['data']
         summary = {}
         #present the games playing and ended
-        on_count, off_count = 0, 0
+        play_count, end_count, wait_count = 0, 0, 0
         summary['total'] = len(games)
         games_detail = []
         #look up every game
         for game in games:
             game_data = {}
+            #game status
             game_data['status'] = game['status_cn']
+            #away team name
             game_data['a_team'] = game['team1_name']
+            #home team name
             game_data['h_team'] = game['team2_name']
+            #away team short name
             game_data['a_short'] = game['team1_alias']
+            #home team short name
             game_data['a_short'] = game['team2_alias']
+            #away team score
             game_data['a_score'] = game['team1_score']
+            #home team score
             game_data['h_score'] = game['team2_score']
             if u'完场'.encode('utf-8') == game['status_cn'].encode('utf-8'):
-                off_count += 1
+                end_count += 1
+            elif u'未开始'.encode('utf-8') == game['status_cn'].encode('utf-8'):
+                wait_count += 1
             else:
-                on_count += 1
+                play_count += 1
             games_detail.append(game_data)
-        summary['on'] = on_count
-        summary['off'] = off_count
+        summary['end'] = end_count
+        summary['wait'] = wait_count
+        summary['play'] = play_count
         summary['detail'] = games_detail
+        #set the flag True when all games over
+        if end_count == summary['total']:
+            all_end = True
         return summary
+
+if __name__ == "__main__":
+    """main method for test"""
+    game = SinaGameDataGetter("2016-11-24")
+    print game.get_games_summary()['detail'][0]['status']
